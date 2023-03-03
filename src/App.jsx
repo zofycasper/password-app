e315d8c6fb5beb871f70b46b5c2020d9872ac8e8
zofycasper <zofycasper@gmail.com>
2023-03-03 15:49:09 +1300

    basic layout html + css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import characters from "./characters";
+import "./App.css";
 
 export default function App() {
-    return <div>This is App</div>;
+    function getString() {
+        return characters.join("");
+    }
+
+    return (
+        <div className="app">
+            <header>
+                <h2 className="title">
+                    <span>Generate a </span>
+                    <br /> random password
+                </h2>
+                <h3 className="subtitle">
+                    Never use an insecure password again.
+                </h3>
+            </header>
+            <button>Generate passwords</button>
+
+            <div className="password-container">
+                <p className="first-password"></p>
+                <p className="second-password"></p>
+            </div>
+        </div>
+    );
 }
